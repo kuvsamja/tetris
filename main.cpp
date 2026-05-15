42ce96ca1702514b591d44e8772f4bd38400eaac
kuvsamja <kuv.cizal.2011@gmail.com>
2026-05-15 21:13:13 +0200

    foundational classes, piece rotation, basic rendering

---
+++ main.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+#include <ncurses.h>
+#include "vec2.h"
+
+
+
+class Piece {
+  public:
+    std::vector<point2<int>> points;
+    point2<double> rotation_point;
+
+    Piece() {}
+
+    Piece(std::vector<point2<int>> points, point2<double> rotation_point) {
+        this->points = points;
+        this->rotation_point = rotation_point;
+    }
+
+    Piece* rotateClockwise() {  // rotates the points vector around the rotation_point; returns pointer to itself
+        std::vector<point2<int>> rotated_points;
+        for(auto point : points) {
+            auto new_point = static_cast<point2<double>>(point) - rotation_point;
+            new_point = point2<double>{-new_point.y(), new_point.x()};
+            new_point += rotation_point;
+            
+            rotated_points.push_back(static_cast<vec2<int>>(new_point));
+        }
+
+        this->points = rotated_points;
+
+        return this;
+    }
+
+
+};
+
+struct Block {
+    uint8_t is_occupied = 0; // 0 - not occupied
+                          // 1 - occupied by a normal block
+                          // 2 - occupied by a currently active piece
+
+};
+
+class Grid {
+  private:
+    std::vector<std::vector<Block>> grid{20, std::vector<Block>(10, {0})};
+    Piece current_piece;
+    Piece next_piece;
+    point2<int> piece_position;
+    int piece_fall_timer = 10; // time it takes for a piece to descend 1 block in ms
+    int input_cooldown_time = 100; // time it takes to move a piece again after holding a key in ms
+    uint64_t timer = 0;
+
+  public:
+    Grid() {}
+
+
+    Piece getRandomPiece() {
+        int piece = rand() % 7;
+        switch(piece) {
+            case 0:
+                return Piece(
+                    std::vector<point2<int>>{{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+                    point2<double>{1.5, 1.5}
+                );
+            case 1:
+                return Piece(
+                    std::vector<point2<int>>{{0, 0}, {0, 1}, {1, 1}, {2, 1}},
+                    point2<double>{1, 1}
+                );
+            case 2:
+                return Piece(
+                    std::vector<point2<int>>{{0, 1}, {1, 1}, {2, 1}, {2, 0}},
+                    point2<double>{1, 1}
+                );
+            case 3:
+                return Piece(
+                    std::vector<point2<int>>{{0, 0}, {0, 1}, {1, 1}, {1, 0}},
+                    point2<double>{0.5, 0.5}
+                );
+            case 4:
+                return Piece(
+                    std::vector<point2<int>>{{0, 1}, {1, 0}, {1, 1}, {2, 0}},
+                    point2<double>{1, 1}
+                );
+            case 5:
+                return Piece(
+                    std::vector<point2<int>>{{0, 1}, {1, 0}, {1, 1}, {2, 1}},
+                    point2<double>{1, 1}
+                );
+            case 6:
+                return Piece(
+                    std::vector<point2<int>>{{0, 0}, {1, 0}, {1, 1}, {2, 1}},
+                    point2<double>{1, 1}
+                );
+        }
+        exit(-1);
+    }
+
+    void gameInit() {
+        initscr();
+        cbreak();
+        noecho();
+        keypad(stdscr, TRUE);
+        nodelay(stdscr, TRUE);
+        curs_set(0);
+        next_piece = getRandomPiece();
+        current_piece = getRandomPiece();
+    }
+
+
+    void render() {
+        clear();
+
+        for(auto row : grid) {
+            for(auto block : row) {
+                switch(block.is_occupied) {
+                    case 0:
+                        printw("--");
+                        break;
+                    case 1:
+                        printw("++");
+                        break;
+                    case 2:
+                        printw("##");
+                        break;
+                }
+            }
+            printw("\n");
+        }
+
+        refresh();
+    }
+
+
+
+
+    void update(int pressed_key) {
+        // current_piece = next_piece;
+        // next_piece = getRandomPiece();
+        timer++;
+
+        for(auto& row : grid) {
+            for(auto& block : row) {
+                if(block.is_occupied == 2) block.is_occupied = 0;
+            }
+        }
+
+
+        for(auto block : current_piece.points) {
+            grid[block.x() + piece_position.x()][block.y() + piece_position.y()].is_occupied = 2;
+        }
+
+        if(timer % 30 == 0) {
+            current_piece.rotateClockwise();
+        }
+        if(timer % 180 == 0) {
+            current_piece = getRandomPiece();
+        }
+
+    }
+
+};
+
+
+int main() {
+    srand(time(NULL));
+
+    Grid grid;
+    grid.gameInit();
+    bool inputs[6] {};
+
+    bool running = true;
+    while(running) {
+        int pressed_key = getch();
+        grid.update(pressed_key);
+        grid.render();
+
+        usleep(16000);
+    }
+    
+
+    endwin();
+    return 0;
+}
---
+++ vec2.h
@@ -0,0 +1,87 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <vector>
+
+template <typename T> 
+class vec2 {
+  public:
+    T e[2];
+    
+    vec2() : e{0,0} {}
+    vec2(T e0, T e1) : e{e0, e1} {}
+
+    T x() const { return e[0]; }
+    T y() const { return e[1]; }
+
+    vec2 operator-() const { return vec2(-e[0], -e[1]); }
+    T operator[](int i) const { return e[i]; }
+    T& operator[](int i) { return e[i]; }
+
+    vec2& operator+=(const vec2& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        return *this;
+    }
+
+    vec2& operator*=(T t) {
+        e[0] *= t;
+        e[1] *= t;
+        return *this;
+    }
+
+    vec2& operator/=(T t) {
+        return *this *= 1/t;
+    }
+
+    bool operator==(const vec2& other) const {
+        return this->x() == other.x() && this->y() == other.y();
+    }
+    template <typename U>
+    explicit operator vec2<U>() const {
+        return vec2<U>(static_cast<U>(e[0]), static_cast<U>(e[1]));
+    }
+};
+
+template <typename T>
+using point2 = vec2<T>;
+
+template <typename T>
+inline vec2<T> operator+(const vec2<T>& u, const vec2<T>& v) {
+    return vec2<T>(u.e[0] + v.e[0], u.e[1] + v.e[1]);
+}
+
+template <typename T>
+inline vec2<T> operator-(const vec2<T>& u, const vec2<T>& v) {
+    return vec2<T>(u.e[0] - v.e[0], u.e[1] - v.e[1]);
+}
+
+template <typename T>
+inline vec2<T> operator*(const vec2<T>& u, const vec2<T>& v) {
+    return vec2<T>(u.e[0] * v.e[0], u.e[1] * v.e[1]);
+}
+
+template <typename T>
+inline vec2<T> operator*(T t, const vec2<T>& v) {
+    return vec2<T>(t*v.e[0], t*v.e[1]);
+}
+
+template <typename T>
+inline vec2<T> operator*(const vec2<T>& v, T t) {
+    return t * v;
+}
+
+template <typename T>
+inline vec2<T> operator/(const vec2<T>& v, T t) {
+    return (1/t) * v;
+}
+
+template <typename T>
+inline vec2<T> abs(const vec2<T>& v) {
+    return vec2<T>(std::abs(v.x()), std::abs(v.y()));
+}
+
+
+
+#endif
